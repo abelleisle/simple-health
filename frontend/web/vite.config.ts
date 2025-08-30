c2d241d6d0f3aa98a5ea2a84c1b01bfc3c7918c6
abelleisle <abelleisle@protonmail.com>
2025-08-30 12:54:19 -0700

    Added login page
    * Gotta learn how to use vite

--- frontend/web/vite.config.ts
+++ frontend/web/vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  // build: {
+  //   rollupOptions: {
+  //     input: {
+  //       main: resolve(__dirname, 'src/index.html'),
+  //       login: resolve(__dirname, 'src/login.html')
+  //     }
+  //   }
+  // },
+  root: 'src'
 })
